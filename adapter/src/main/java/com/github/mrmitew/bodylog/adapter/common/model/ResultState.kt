c05b6928ea8c9559c0ed79796ea9743173ca39f9
Stefan Mitev <mr.mitew@gmail.com>
2017-09-12 17:44:10 +0200

    [rem] unnecessary code

--- adapter/src/main/java/com/github/mrmitew/bodylog/adapter/common/model/ResultState.kt
+++ adapter/src/main/java/com/github/mrmitew/bodylog/adapter/common/model/ResultState.kt
@@ -1,22 +1,3 @@
 package com.github.mrmitew.bodylog.adapter.common.model
 
 open class ResultState(open val isInProgress: Boolean, open val isSuccessful: Boolean, open val error: Throwable)
-
-object Factory {
-    internal fun inProgress(): ResultState =
-            ResultState(
-                    isInProgress = true,
-                    isSuccessful = false,
-                    error = StateError.Empty.INSTANCE)
-
-    internal fun successful(): ResultState =
-            ResultState(
-                    isInProgress = false,
-                    isSuccessful = true,
-                    error = StateError.Empty.INSTANCE)
-
-    internal fun error(throwable: Throwable): ResultState =
-            ResultState(isInProgress = false,
-                    isSuccessful = false,
-                    error = throwable)
-}
